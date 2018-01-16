8729ee8035ed8a16bc7f6de197d3098f73d82ca4
BULGARE Gregory <leangeder@hotmail.com>
2018-01-16 07:28:05 +0000

    backup

--- setup.sh
+++ setup.sh
@@ -27,3 +27,39 @@ for file in $(ls ${SCRIPT}/tmux); do
    fi
    ln -s ${SCRIPT}/tmux/${file} ${HOME}/.${file}
 done
+
+for file in $(ls ${SCRIPT}/ssh); do
+   #if [ -e "${HOME}/.${file}" ]
+   if [ -e "${HOME}/.ssh" ]
+   then
+      rm -fr ${HOME}/.ssh
+   fi
+   ln -s ${SCRIPT}/ssh ${HOME}/.ssh
+done
+
+for file in $(ls ${SCRIPT}/awesome); do
+   #if [ -e "${HOME}/.config/${file}" ]
+   if [ -e "${HOME}/.config/awesome" ]
+   then
+      rm -fr ${HOME}/.config/awesome
+   fi
+   ln -sf ${SCRIPT}/awesome ${HOME}/.config/awesome
+done
+
+for file in $(ls ${SCRIPT}/mpd); do
+   #if [ -e "${HOME}/.config/${file}" ]
+   if [ -e "${HOME}/.config/mpd" ]
+   then
+      rm -fr ${HOME}/.config/mpd
+   fi
+   ln -sf ${SCRIPT}/mpd ${HOME}/.config/mpd
+done
+
+for file in $(ls ${SCRIPT}/ncmpcpp); do
+   #if [ -e "${HOME}/.config/${file}" ]
+   if [ -e "${HOME}/.config/ncmpcpp" ]
+   then
+      rm -fr ${HOME}/.config/ncmpcpp
+   fi
+   ln -sf ${SCRIPT}/ncmpcpp ${HOME}/.config/ncmpcpp
+done
